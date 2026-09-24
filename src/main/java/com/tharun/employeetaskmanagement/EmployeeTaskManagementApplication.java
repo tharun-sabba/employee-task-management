1d546b05d6f4679075b797cef3411ab948795ac4
tharun-sabba <sabbatharun10@gmail.com>
2026-09-24 17:04:49 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/tharun/employeetaskmanagement/EmployeeTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tharun.employeetaskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTaskManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tharun/employeetaskmanagement/EmployeeTaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tharun.employeetaskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
